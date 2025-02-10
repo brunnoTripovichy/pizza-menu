24437824e481c57853873fa6c299a9c21fb0fcc1
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-10 17:57:30 -0500

    feat(pizza): scaffolding pizza menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import pizzaData from './data';
 
-function App() {
-  return <h1>Hello World!</h1>;
+const App = () => {
+    return (
+        <div>
+          <Header />
+          <Menu />
+          <Footer />
+        </div>
+  );
+}
+
+const Pizza = () => {
+  return (
+    <div>
+      <img alt="Pizza"></img>
+      <h1>Pizza</h1>
+      <p>Ingridients</p>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Pizzaria Name</h1>
+    </div>
+  );
+}
+
+const Menu = () => {
+  return (
+    <div>
+      <h2>Menu: </h2>
+      <Pizza />
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 // Create a root and render the App component under React 18
