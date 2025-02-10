df1132d7269717d8ba6e8311f76208077a1660d6
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-10 17:15:57 -0500

    feat(pizza): add strict mode

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ function App() {
 
 // Create a root and render the App component under React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// Start the root with the App component wrapped in React.StrictMode
+root.render(<React.StrictMode><App /></React.StrictMode>);
