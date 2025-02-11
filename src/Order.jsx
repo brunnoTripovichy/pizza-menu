df647a835e3e42a48386f5d08889dfa7aa217882
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-11 15:07:01 -0500

    refactor: aria

--- src/Order.jsx
+++ src/Order.jsx
@@ -4,7 +4,9 @@ const Order = ({ closeTime }) => {
   return (
     <div className='order'>
       <p>We are open until {closeTime}:00 PM. Come visit us or order online!</p>
-      <button className='btn'>Order Now</button>
+      <button className='btn' aria-label='Order pizza online now'>
+        Order Now
+      </button>
     </div>
   );
 };
