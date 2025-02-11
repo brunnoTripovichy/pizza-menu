c9a2705591d4aed1098c7e76701839599648e4d6
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-11 15:05:58 -0500

    refactor: move service hours to constant data file

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,19 @@
 import Order from './Order';
+import { OPEN_TIME, CLOSE_TIME } from './data';
 
 const Footer = () => {
   const hour = new Date().getHours();
-  const openTime = 10;
-  const closeTime = 23;
-  const isOpen = hour >= openTime && hour <= closeTime;
+
+  const isOpen = hour >= OPEN_TIME && hour <= CLOSE_TIME;
 
   return (
     <footer className='footer'>
       {isOpen ? (
-        <Order closeTime={closeTime} />
+        <Order closeTime={CLOSE_TIME} />
       ) : (
         <p>
-          We are closed. We open at {openTime}:00 AM. Visit us tomorrow or order
-          online!
+          We are closed. We open at {OPEN_TIME}:00 AM. Visit us tomorrow or
+          order online!
         </p>
       )}
     </footer>
